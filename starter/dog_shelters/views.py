92942dc1eece4392624666c6ddc8959e7845589d
Muhammad Habib <habibsahabubit@gmail.com>
2024-01-24 04:33:05 +0500

    habib's commit

--- starter/dog_shelters/views.py
+++ starter/dog_shelters/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Shelter
+
 
 # Create your views here.
+def shelter_list(request):
+    shelters = Shelter.objects.all()
+    context = { 'shelters': shelters }
+    return render(request, 'shelter_list.html', context)
+
+def shelter_detail(request, pk):
+    # shelter = get_object_or_404(models.Shelter, pk=pk)
+    shelter= Shelter.objects.get(pk=pk)
+    context = {'shelter': shelter}
+    return render(request, 'shelter_detail.html', context)
--- starter/project/urls.py
+++ starter/project/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     # TODO: Register URLconf
 
     path('admin/', admin.site.urls),
+    path('', include('dog_shelters.urls')),
 ]
